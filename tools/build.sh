507350bcadfea574f8a8c2d96e93f832d5ca52b7
xyz <xyzhtml@163.com>
2014-05-26 18:35:27 +0800

    update list and button components

--- tools/build.sh
+++ tools/build.sh
@@ -2,19 +2,21 @@
 rm -rf ./.build; mkdir -p .build;
 cat less/*.less > .build/mob.less
 lessc .build/mob.less .build/mob.css
+
 rm -rf release;
 mkdir -p release/css
 cp .build/mob.css release/css/mob.css ;
-cp .build/mob.css docs/css/mob.css
+#cp .build/mob.css docs/css/mob.css
 cp css/* docs/examples/css/
 cp .build/mob.css docs/examples/css/mob.css
 cat js/*.js > .build/mob.js
 
 mkdir -p release/js
 cp .build/mob.js release/js/mob.js
-cp .build/mob.js docs/js/mob.js
+#cp .build/mob.js docs/js/mob.js
 cp .build/mob.js docs/examples/js/mob.js
 mkdir -p release/img
 cp -r img release/
-cp -r img docs/
+#cp -r img docs/
+cp -r img docs/examples/
 
