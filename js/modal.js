a1551a43fc55b886b11a791bdc00aaa440d93302
xyz <xyzhtml@163.com>
2014-05-28 19:18:38 +0800

    add grunt to build the frame

--- js/modal.js
+++ js/modal.js
@@ -3,54 +3,58 @@
     /**
      * 弹出层和对话框的基础组件  
      * 示例:
-     *      $(".tips").modal();
+     *      $(".model").modal();
      */
-    $.fn.Modal = function(options){
-        
-    }
-    /**
-     * 对话框
-     * 示例:
-     *      $(".dialog").dialog();
-     */
-    $.fn.Dialog = function(options){
-        var me = this;                                                                                 
-        this.$el = options.el || $("#dialogPanel");                                                                  
-        if (options){                                                                                  
-            if (options.$el) this.$el = $(options.$el);                                                
-            if (options.confirm) this.confirm = options.confirm;                                       
-        }                                                                                              
-        this.$el.find(".btn-confirm").bind("click",function(){                                         
-            if(me.confirm){                                                                            
-                me.confirm.call(me,this);                                                              
-            }                                                                                          
-        });                                                                                            
-        this.$el.find(".btn-cancel").bind("click",function(){                                          
-            me.hide();                                                                                 
+    var Model = function(options){
+        var me = this;
+        if (!options) return;
+        if (typeof options == 'string'){
+            this.el = options;
+            this.$el = $(this.el);
+        }else{
+            this.$el = options.$el || $(options.el);
+            this.confirm = options.confirm;                                       
+        }
+        this.$el.find(".btn-confirm").bind("click",function(){
+            if(me.confirm){
+                me.confirm.call(me,this);
+            }
         }); 
-    }
+        this.$el.find(".btn-cancel").bind("click",function(){
+            me.hide(); 
+        });
+    };
 
-    $.fn.Dialog.prototype = {                                                                         
-        pos : function () {                                                                            
-            this.$el.css({                                                                             
-                top     : window.pageYOffset + (window.innerHeight - 85)*0.5 + 'px',                   
-                left    : (window.innerWidth - 216)*0.5 + 'px'                                         
-            })                                                                                         
-        },                                                                                             
-        show : function(options){                                                                      
-            if(this.showed) return;                                                                    
-            this.pos();                                                                                
-            if(options.msg){                                                                           
-                $(".dialog-tip",this.$el).text(options.msg);                                           
-            }                                                                                          
-            this.$el.addClass("show");                                                                 
-            this.showed = true;                                                                        
-        },                                                                                             
-        hide : function(){                                                                             
-            if(!this.showed) return;                                                                   
-            this.$el.removeClass("show");                                                              
-            this.showed = false;                                                                       
-        }                                                                                              
-    };          
+    Model.prototype = {
+        pos : function () {
+            var dialog = this.$el.find('.modal-dialog');
+            console.log(dialog);
+                dialog.css({
+                    //top     : window.pageYOffset + (window.innerHeight - 85)*0.5 + 'px',
+                    //left    : (window.innerWidth - 216)*0.5 + 'px'
+                })
+        },
+        show : function(options){ 
+            if(this.showed) return;
+            this.pos();
+            if(options && options.msg){
+                $(".dialog-tip",this.$el).text(options.msg);
+            }
+            this.$el.removeClass("hide").addClass("show");
+            this.showed = true;
+            $('body').append('<div class="modal-backdrop"></div>');
+        }, 
+        hide : function(){
+            if(!this.showed) return;
+            this.$el.removeClass("show").addClass("hide");
+            $('.modal-backdrop').remove();
+            this.showed = false;
+        }
+    };
+
+    $.fn.modal = function(options){
+        var modal = new Model(options);
+        return modal
+    }
 
 }($, window, document));
